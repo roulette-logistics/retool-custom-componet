e8f4c896f94a2bc1291829f6d0c81a3234eba760
sathithyayogi <sathithyayogi99@gmail.com>
2023-11-14 16:27:06 +0530

    styled component conversion

--- src/WhereCondition.js
+++ src/WhereCondition.js
@@ -12,7 +12,6 @@ import RightSideComponent from "./RightSideComponent";
 import SelectComponent from "./SelectComponent";
 import styled from "styled-components";
 
-
 const FieldSelectorParentWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -47,180 +46,196 @@ const ColumnSelectionWrapper = styled.div`
   gap: 15px;
 `;
 
+const WholeWrapper = styled.div`
+  width: 100%;
+`
+
+const SelectComponentWrapper = styled.div`
+  flex-basis: 35%;
+`
+
+const SelectComponentOperatorWrapper = styled.div`
+  flex-basis: 25%;
+`
+const DeleteIconWrapper = styled.div`
+  flex-basis: 5%;
+  right: 0;
+`
+
 const WhereCondition = ({ triggerQuery, model, modelUpdate }) => {
 
-    const fieldOptionData = model?.columnsData?.map((data) => {
-        return {
-          value: data.name,
-          label: data.name,
-          dataType: data.dataType,
-          disabled: !data.isSupported, // if it is not supported then disable it
-        };
-      });
-      
-    const defaultArrayValue = {
-        columnsListArray: fieldOptionData || model.outputData?.filterDropDownData?.[0]?.columnsListArray ||[],
-        operatorArrayData: [],
-        column: "",
-        operator: OperatorData[0],
-        value: "",
-        betweenValue: whereConditionOperator[0],
-        dataType: ""
-      };
-    
-      const { register, control, handleSubmit, watch, setValue, reset } = useForm({
-        defaultValues: {
-          filterDropDownData: [defaultArrayValue],
-        },
-      });
-    
-      const { fields, append, remove } = useFieldArray({
-        control,
-        name: "filterDropDownData",
-      });
-    
+  const fieldOptionData = model?.columnsData?.map((data) => {
+    return {
+      value: data.name,
+      label: data.name,
+      dataType: data.dataType,
+      disabled: !data.isSupported, // if it is not supported then disable it
+    };
+  });
 
-    useEffect(() => {
-        if (model.isBtnClicked == true) {
-          onSubmit();
-        }
-    
-        if (model.isEdit == true) {
-          reset(model.outputData);
-          modelUpdate({
-            isEdit: false,
-          });
-        }
-      }, [model]);
-    
+  const defaultArrayValue = {
+    columnsListArray: fieldOptionData || model.outputData?.filterDropDownData?.[0]?.columnsListArray || [],
+    operatorArrayData: [],
+    column: "",
+    operator: OperatorData[0],
+    value: "",
+    betweenValue: whereConditionOperator[0],
+    dataType: ""
+  };
 
-    const onSubmit = () => {
-        const formData = watch();
-        modelUpdate({
-          isBtnClicked: false,
-          outputData: formData,
-        });
-    
-        triggerQuery("get_query_payload");
-      };
-    
+  const { register, control, handleSubmit, watch, setValue, reset } = useForm({
+    defaultValues: {
+      filterDropDownData: [defaultArrayValue],
+    },
+  });
+
+  const { fields, append, remove } = useFieldArray({
+    control,
+    name: "filterDropDownData",
+  });
+
+
+  useEffect(() => {
+    if (model.isBtnClicked == true) {
+      onSubmit();
+    }
+
+    if (model.isEdit == true) {
+      reset(model.outputData);
+      modelUpdate({
+        isEdit: false,
+      });
+    }
+  }, [model]);
+
+
+  const onSubmit = () => {
+    const formData = watch();
+    modelUpdate({
+      isBtnClicked: false,
+      outputData: formData,
+    });
+
+    triggerQuery("get_query_payload");
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-    <FieldSelectorParentWrapper>
-      <OperatorWrapper
-      >
-        {fields.map((item, index) => {
-          return (
-            <>
-              <div key={index} style={{ width: "100%" }}>
-                {index !== 0 ? (
-                  <FieldSelection>
-                    <SelectComponent
-                      name={`filterDropDownData[${index}].betweenValue`}
-                      value={
-                        watch()?.filterDropDownData[index]?.betweenValue || {}
-                      }
-                      control={control}
-                      options={whereConditionOperator}
-                      onChange={(betweenValue) => {
-                        setValue(
-                          `filterDropDownData[${index}].betweenValue`,
-                          betweenValue
-                        );
-                      }}
-                    />
-                  </FieldSelection>
-                ) : null}
-
-                <ColumnSelectionWrapper
-                >
-                  <div style={{ flexBasis: "35%" }}>
-                    <SelectComponent
-                      control={control}
-                      placeholder="Select Column"
-                      value={watch()?.filterDropDownData?.[index]?.column || {}}
-                      name={`filterDropDownData[${index}].column`}
-                      options={
-                        watch()?.filterDropDownData[index]?.columnsListArray
-                      }
-                      onChange={(column) => {
-                        setValue(`filterDropDownData[${index}].value`, "");
-                        setValue(`filterDropDownData[${index}].dataType`, column.dataType);
-                        setValue(
-                          `filterDropDownData[${index}].column`,
-                          column
-                        );
-                      }}
-                    />
-                  </div>
-
-                  <div style={{ flexBasis: "25%" }}>
-                    <SelectComponent
-                      name={`filterDropDownData[${index}].operator`}
-                      value={watch()?.filterDropDownData?.[index]?.operator || {}}
-                      control={control}
-                      options={getSupoortedOperators(watch()?.filterDropDownData[index]?.dataType || [])}
-                      onChange={(operator) => {
-                        setValue(
-                          `filterDropDownData[${index}].operator`,
-                          operator
-                        );
-                      }}
-                    />
-                  </div>
-
-                  {watch()?.filterDropDownData[index]?.operator?.value ==
-                    "isNull" ? <div style={{ flexBasis: "35%" }}></div> : (
-                    <RightSideComponent 
-                    name={`filterDropDownData[].value`}
-                    watch={watch} 
-                    control={control} 
-                    setValue={setValue} 
-                    index={index} 
-                    register={register} 
-                    dataType={watch()?.filterDropDownData[index].column?.dataType}
-                    operator={watch()?.filterDropDownData[index]?.operator?.value}
-                    />
-                  )}
-
-                  <div style={{ flexBasis: "5%", right:0 }}>
-                    <RiDeleteBinLine
-                      className="pointer"
-                      color="#B42318"
-                      size={"20"}
-                      onClick={() => {
-                        remove(index);
-                      }}
-                    />
-                  </div>
-
-                </ColumnSelectionWrapper>
-              </div>
-            </>
-          );
-        })}
-      </OperatorWrapper>
-
-      <section>
-        <Button
-          style={{
-            color: "#3054B9",
-            textTransform: "none",
-            fontSize: "14px",
-            fontWeight: "600",
-            fontFamily: "Inter",
-          }}
-          onClick={() => {
-            append(defaultArrayValue);
-          }}
-          startIcon={<AiOutlinePlus />}
-          variant="text"
+      <FieldSelectorParentWrapper>
+        <OperatorWrapper
         >
-          Add Condition
-        </Button>
-      </section>
-    </FieldSelectorParentWrapper>
-  </form>
+          {fields.map((item, index) => {
+            return (
+              <>
+                <WholeWrapper index={index}>
+                  {index !== 0 ? (
+                    <FieldSelection>
+                      <SelectComponent
+                        name={`filterDropDownData[${index}].betweenValue`}
+                        value={
+                          watch()?.filterDropDownData[index]?.betweenValue || {}
+                        }
+                        control={control}
+                        options={whereConditionOperator}
+                        onChange={(betweenValue) => {
+                          setValue(
+                            `filterDropDownData[${index}].betweenValue`,
+                            betweenValue
+                          );
+                        }}
+                      />
+                    </FieldSelection>
+                  ) : null}
+
+                  <ColumnSelectionWrapper
+                  >
+                    <SelectComponentWrapper>
+                      <SelectComponent
+                        control={control}
+                        placeholder="Select Column"
+                        value={watch()?.filterDropDownData?.[index]?.column || {}}
+                        name={`filterDropDownData[${index}].column`}
+                        options={
+                          watch()?.filterDropDownData[index]?.columnsListArray
+                        }
+                        onChange={(column) => {
+                          setValue(`filterDropDownData[${index}].value`, "");
+                          setValue(`filterDropDownData[${index}].dataType`, column.dataType);
+                          setValue(
+                            `filterDropDownData[${index}].column`,
+                            column
+                          );
+                        }}
+                      />
+                    </SelectComponentWrapper>
+
+                    <SelectComponentOperatorWrapper>
+                      <SelectComponent
+                        name={`filterDropDownData[${index}].operator`}
+                        value={watch()?.filterDropDownData?.[index]?.operator || {}}
+                        control={control}
+                        options={getSupoortedOperators(watch()?.filterDropDownData[index]?.dataType || [])}
+                        onChange={(operator) => {
+                          setValue(
+                            `filterDropDownData[${index}].operator`,
+                            operator
+                          );
+                        }}
+                      />
+                    </SelectComponentOperatorWrapper>
+
+                    {watch()?.filterDropDownData[index]?.operator?.value ==
+                      "isNull" ? <SelectComponentWrapper></SelectComponentWrapper> : (
+                      <RightSideComponent
+                        name={`filterDropDownData[].value`}
+                        watch={watch}
+                        control={control}
+                        setValue={setValue}
+                        index={index}
+                        register={register}
+                        dataType={watch()?.filterDropDownData[index].column?.dataType}
+                        operator={watch()?.filterDropDownData[index]?.operator?.value}
+                      />
+                    )}
+
+                    <DeleteIconWrapper>
+                      <RiDeleteBinLine
+                        className="pointer"
+                        color="#B42318"
+                        size={"20"}
+                        onClick={() => {
+                          remove(index);
+                        }}
+                      />
+                    </DeleteIconWrapper>
+
+                  </ColumnSelectionWrapper>
+                </WholeWrapper>
+              </>
+            );
+          })}
+        </OperatorWrapper>
+
+        <section>
+          <Button
+            style={{
+              color: "#3054B9",
+              textTransform: "none",
+              fontSize: "14px",
+              fontWeight: "600",
+              fontFamily: "Inter",
+            }}
+            onClick={() => {
+              append(defaultArrayValue);
+            }}
+            startIcon={<AiOutlinePlus />}
+            variant="text"
+          >
+            Add Condition
+          </Button>
+        </section>
+      </FieldSelectorParentWrapper>
+    </form>
   )
 }
 
